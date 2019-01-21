3eb616e13bbd4241912d3e12458e2c73c391575d
Erik Nytting <erik.nytting@tiqqe.com>
2019-01-21 20:43:23 +0800

    added custom keys map

--- LogEntry.go
+++ LogEntry.go
@@ -6,10 +6,11 @@ import (
 
 // LogEntry default log entry
 type LogEntry struct {
-	Message      string `json:"message"`
-	ErrorMessage string `json:"errorMessage,omitempty"`
-	RequestID    string `json:"requestId"`
-	LogLevel     string `json:"logLevel"`
+	Message      string                 `json:"message"`
+	ErrorMessage string                 `json:"errorMessage,omitempty"`
+	RequestID    string                 `json:"requestId"`
+	LogLevel     string                 `json:"logLevel"`
+	Keys         map[string]interface{} `json:"keys,omitempty"`
 }
 
 // Stringify marshal json to string
@@ -28,3 +29,11 @@ func (e *LogEntry) SetLogLevel(level string) { e.LogLevel = level }
 
 // SetRequestID sets the request id
 func (e *LogEntry) SetRequestID(id string) { e.RequestID = id }
+
+// SetKey sets the value for the custom key
+func (e *LogEntry) SetKey(key string, value interface{}) {
+	if e.Keys == nil {
+		e.Keys = make(map[string]interface{})
+	}
+	e.Keys[key] = value
+}
